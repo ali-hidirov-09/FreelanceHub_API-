12a04e521cc28a8b65bee5d26c0f6dd77520ea71
Khidirov Ali <alihidirov285@gmail.com>
2026-06-07 21:53:46 +0500

    main.py moved into app

---
+++ app/main.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI
+from .routers import api_router
+
+app = FastAPI(
+    title="Freelance Hub API",
+    summary="Project for freelansers and users",
+    description="""
+    Bu API lar Freelanserlar va zakaz beruvchilarni bog'lash uchun yaratilgan
+    
+    Features:
+    - Jobs
+    """,
+    version="1.0.1",
+
+    # docs_url
+    docs_url="/docs",
+    redoc_url="/redoc",
+    openapi_url="/openapi.json",
+
+    contact={
+        "name": "Ali",
+        "email": "alihidirov285@gmail.com",
+        "url": "https://github.com/ali-hidirov-09"
+    },
+
+    # license
+    license_info={
+        "name": "MIT",
+        "identifier": "MIT"
+    },
+
+
+    # swagger tags order / groups
+    openapi_tags=[
+        {
+            "name": "Jobs",
+            "description": "Job management"
+        }
+    ],
+
+
+    # swagger ui settings
+    # swagger_ui_parameters={
+    #     "defaultModelsExpandDepth": -1,
+    #     "docExpansion": "list",
+    #     "displayRequestDuration": True,
+    #     "filter": True
+    # },
+
+    # deprecated api hide
+    deprecated=False,
+
+    # root path (reverse proxy uchun)
+    root_path="",
+
+    # openapi version
+    openapi_version="3.1.0"
+)
+
+app.include_router(api_router, prefix="/api/v1")
