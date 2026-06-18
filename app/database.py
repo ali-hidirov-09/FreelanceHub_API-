69797b220cc05b11a03dc343572b703510cb4435
Khidirov Ali <alihidirov285@gmail.com>
2026-06-18 19:52:04 +0500

    database.py developed

--- app/database.py
+++ app/database.py
@@ -1,7 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent
 
-sqlite_url = "sqlite:///./freelance_hub.db"
+PROJECT_ROOT = BASE_DIR.parent
+
+DB_PATH = PROJECT_ROOT / "freelance_hub.db"
+
+sqlite_url = f"sqlite:///{DB_PATH}"
 
 engine = create_engine(sqlite_url, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
