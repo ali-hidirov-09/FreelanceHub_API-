529e6f42732535a11918d96dc405a1c7269a0691
Khidirov Ali <alihidirov285@gmail.com>
2026-06-18 19:49:00 +0500

    learnt sqlalchemy and wrote codes with it

--- app/database.py
+++ app/database.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 sqlite_url = "sqlite:///./freelance_hub.db"
 
 engine = create_engine(sqlite_url, echo=True)
-
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 class Base(DeclarativeBase):
     pass
