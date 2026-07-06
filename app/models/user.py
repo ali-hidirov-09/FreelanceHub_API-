70050f5c6b586aca2e3c0b67e63f451782f0d149
Khidirov Ali <alihidirov285@gmail.com>
2026-07-06 13:02:37 +0500

    wrote code with RBAC

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,6 @@
 import enum
 from sqlalchemy import String, text, Enum
 from app.core.database import Base
-
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import datetime
 from .job import Job
@@ -19,12 +18,11 @@ class User(Base):
     email: Mapped[str] = mapped_column(String(100), unique=True, index=True)
     full_name: Mapped[str] = mapped_column(String(100))
     hashed_password: Mapped[str] = mapped_column(String(100))
-    role: Mapped[Role] = mapped_column(Enum(Role, native_enum=False), default=Role.EMPLOYER)
+    role: Mapped[Role] = mapped_column(Enum(Role, native_enum=True), default=Role.FREELANCER)
     is_active: Mapped[bool] = mapped_column(default=True)
     created_at: Mapped[datetime] = mapped_column(server_default=text("CURRENT_TIMESTAMP"))
 
-    jobs: Mapped[list["Job"]] = relationship(back_populates="user")
-
+    jobs: Mapped[list["Job"]] = relationship(back_populates="user", passive_deletes=True)
 
     def __repr__(self):
         return f"""
